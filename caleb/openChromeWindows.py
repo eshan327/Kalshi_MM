9368f1c0c7e9864a33dcbe2d595f8b48e1fb9c2f
Caleb Chang <calebschang13@gmail.com>
2025-04-02 20:10:48 -0400

    chrome webdriver

---
+++ caleb/openChromeWindows.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+market_url  = "https://demo.kalshi.co/markets/kxwiscotus/wi-state-supreme-court"
+
+import subprocess
+import time
+
+
+chrome_path = r"your path to\chrome.exe"
+
+
+subprocess.Popen([
+    chrome_path, 
+    "--remote-debugging-port=9222", 
+    "--user-data-dir=C:\\selenium_chrome_profile"
+], shell=False)
+
+
+time.sleep(5)
